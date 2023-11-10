3b7fb1541b7598a5607727954b9042297728c7b8
Nepte <51161392+Nepte@users.noreply.github.com>
2023-11-10 23:59:49 +0100

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+
+namespace Hangman
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void NewGameButton_Click(object sender, RoutedEventArgs e)
+        {
+            HangmanGameWindow gameWindow = new HangmanGameWindow();
+            gameWindow.Show();
+            this.Close();
+        }
+    }
+}
